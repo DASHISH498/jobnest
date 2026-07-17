9b13e39d7b44b561f74c482e7a8ee31b3b57317e
Ashish Kumar Soni <106304040+DASHISH498@users.noreply.github.com>
2026-07-17 23:12:52 +0530

    app router basename added for git live

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -9,26 +9,31 @@ import HomePage from "../pages/HomePage";
 import JobsPage from "../pages/JobsPage";
 import JobDetailsPage from "../pages/JobDetailsPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "jobs",
+          element: <JobsPage />,
+        },
+        {
+          path: "jobs/:id",
+          element: <JobDetailsPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "jobs",
-        element: <JobsPage />,
-      },
-      {
-        path: "jobs/:id",
-        element: <JobDetailsPage />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.PROD ? "/jobnest" : "/",
+  }
+);
 
 export default function AppRouter() {
   return <RouterProvider router={router} />;
